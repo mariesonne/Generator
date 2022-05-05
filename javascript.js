38096866594cb9c30479f5be6635f405e4a0a60a
Marie Sonne <71631052+mariesonne@users.noreply.github.com>
2022-05-05 12:42:12 +0200

    Tilføjelse af strenge i array
    Jeg har tilføjet 3 strenge og er klar til at test generatoren på siden

--- javascript.js
+++ javascript.js
@@ -1,7 +1,10 @@
 var guides = [
-    '<h3>Cardigan</h3>' + 'Tjek ' + '<a href="http://nordicskyyarn.com/index.php/oulu/">oulu</a>' + ' ud',
-    '<h3>Socks</h3>' + 'Use the ',
-    'Hand dye your own yarn'
+    '<h3>A cardigan!</h3>' + 'Challenge time! Make a cardigan ONLY out of our' + '<a href="http://nordicskyyarn.com/index.php/oulu/"> Oulu</a>' + ' yarn',
+    
+    '<h3>Socks</h3>' + 'Make your own comfy socs. Check out our ' + '<a href="http://nordicskyyarn.com/index.php/bergen/">Bergen</a>' + ' yarn, if you want socks with a nice play of colors!',
+    
+    '<h3>Hand dye your own yarn</h3>' + 'It\'s super fun and very easy! Just follow our ' + '<a href="http://nordicskyyarn.com/index.php/hand-dyeing-yarn/">guide</a>' + ' and create your own interpretation of the Nordic spirit.',
+    
 ]
 
 function newGuide() {
