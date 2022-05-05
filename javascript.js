5f337c3b85ccc14a2e21b6aba412d0e07be1bcdf
Marie Sonne <71631052+mariesonne@users.noreply.github.com>
2022-05-05 12:10:34 +0200

    Styling
    Generatoren er stylet så den passer ind på hjemmesiden.

--- javascript.js
+++ javascript.js
@@ -1,6 +1,6 @@
 var guides = [
     '<h3>Cardigan</h3>' + 'Tjek ' + '<a href="http://nordicskyyarn.com/index.php/oulu/">oulu</a>' + ' ud',
-    'Socks',
+    '<h3>Socks</h3>' + 'Use the ',
     'Hand dye your own yarn'
 ]
 
