9e75f0746e760243a1eb45ad367e8e852fe7fed5
Marie Sonne <71631052+mariesonne@users.noreply.github.com>
2022-05-06 10:08:01 +0200

    Flere strenge tilføjet

--- javascript.js
+++ javascript.js
@@ -5,6 +5,17 @@ var guides = [
     
     '<h3>Hand dye your own yarn</h3>' + 'It\'s super fun and very easy! Just follow our ' + '<a href="http://nordicskyyarn.com/index.php/hand-dyeing-yarn/">guide</a>' + ' and create your own interpretation of the Nordic spirit.',
     
+    '<h3>A soft scarf</h3>' + 'What\'s better than a super soft scarf? ' + 'We actually have a ' + '<a href="http://nordicskyyarn.com/index.php/soft-pennant-scarf/">guide</a>' + ' for that!',
+    
+    '<h3>Baby blanket!</h3>' + 'Try mixing ' + '<a href="http://nordicskyyarn.com/index.php/oulu/">Oulu</a>' + ' and ' + '<a href="http://nordicskyyarn.com/index.php/bergen/">Bergen</a>' + ' for a soft and colourful result!',
+    
+    '<h3>A scrunchie!</h3>' + 'It\'s a super fun small project! If you have any yarn "leftovers", feel free to use them!',
+    
+    '<h3>Make a simple hat!</h3>' + 'You can make a basic white out of ' + '<a href="http://nordicskyyarn.com/index.php/sundsvall/">Sundsvall</a>' + ' or hand-dye it in your favorite colors!'
+    
+    
+    
+    
 ]
 
 function newGuide() {
